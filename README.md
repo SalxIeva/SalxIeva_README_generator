54181afb51f46f92aa0457652d833f970e0d77c0
SalxIeva <ieva.saldukaite@gmail.com>
2023-12-17 19:14:58 +0000

    generateMarkdown return content added

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # README generator
 
+  ## Description
+  This project is made to help write high quality README file
+  
+  ## Table of Contents
+  
+  
+  ## Installation
+  You will need to have node.js installed and then you can just simply use it through termianl in VScode
+  
+  ## Usage
+  
+  
+  ## License
+  This project is licensed under the MIT License.
+  
+  ## Contributing
+  
+  
+  ## Tests
+  tyoe node index.js to the terminal
+  
+  ## Questions
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,30 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  ${data.tableOfContents.map(section => `- [${section}](#${section.toLowerCase()})`).join('\n')}
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} License.
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  ${data.questions}
+  `;
 }
 
 module.exports = generateMarkdown;
